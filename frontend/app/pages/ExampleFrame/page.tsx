48c56caca0f75b87e976564cafb70fb416509360
gconnect <agatevureglory@gmail.com>
2024-06-16 21:52:38 +0100

    add missing dependencies and frame

--- frontend/app/pages/ExampleFrame/page.tsx
+++ frontend/app/pages/ExampleFrame/page.tsx
@@ -1,33 +1,44 @@
-import { FrameMetadata, getFrameMetadata } from '@coinbase/onchainkit/frame';
- 
+import { getFrameMetadata } from '@coinbase/onchainkit';
+import type { Metadata } from 'next';
+
+const { NEXT_PUBLIC_URL} = process.env
+
+const frameMetadata = getFrameMetadata({
+  buttons: [
+    {
+      label: 'Trending Crypto Pools',
+    },
+    {
+      action: 'link',
+      label: 'Visit Coingcko',
+      target: 'https://www.coingecko.com',
+    },
+  ],
+  image: {
+    src: `https://images.unsplash.com/photo-1643408875993-d7566153dd89?q=80&w=2980&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`,
+    aspectRatio: '1:1',
+  },
+  postUrl: `${NEXT_PUBLIC_URL}/api/trendingPools`,
+});
+
+export const metadata: Metadata = {
+  metadataBase: new URL(NEXT_PUBLIC_URL as string),
+  title: 'Crypto Farcaster Frames - Powered by CoinGecko',
+  description: 'LFG',
+  openGraph: {
+    title: 'Crypto Farcaster Frames - Powered by CoinGecko',
+    description: 'LFG',
+    images: [`https://images.unsplash.com/photo-1643408875993-d7566153dd89?q=80&w=2980&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D`],
+  },
+  other: {
+    ...frameMetadata,
+  },
+};
+
 export default function ExampleFrame() {
   return (
-    <getFrameMetadata
-      buttons={[
-        {
-          label: 'Tell me the story',
-        },
-        {
-          action: 'link',
-          label: 'Link to Google',
-          target: 'https://www.google.com'
-        },
-        {
-          action: 'post_redirect',
-          label: 'Redirect to cute pictures',
-        },
-      ]}
-      image={{
-       src: 'https://zizzamia.xyz/park-3.png',
-       aspectRatio: '1:1'
-      }}
-      input={{
-        text: 'Tell me a boat story',
-      }}
-      state={{
-        counter: 1,
-      }}
-      postUrl="https://zizzamia.xyz/api/frame"
-    />
+    <>
+      <h1>Welcome to Crypto Farcaster Frames - Powered by CoinGecko</h1>
+    </>
   );
 }
