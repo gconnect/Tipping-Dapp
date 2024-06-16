5bdb454dab1700d598cf1bd7d494a1c769380c16
gconnect <agatevureglory@gmail.com>
2024-06-16 22:51:53 +0100

    add missing variables

--- frontend/app/utils/mint.ts
+++ frontend/app/utils/mint.ts
@@ -1,20 +1,21 @@
 import { createWalletClient, http, createPublicClient } from "viem";
 import { privateKeyToAccount } from "viem/accounts";
 import { baseSepolia } from "viem/chains";
-import contractAbi from "../cartesi/rollups.json";
-const contractAddress = process.env.CONTRACT_ADDRESS as `0x`;
+import config from "../cartesi/rollups.json";
 
-const account = privateKeyToAccount((process.env.PRIVATE_KEY as `0x`) || "");
+const contractAddress = config.contracts.ERC20Portal.address as '0x';
+
+const account = privateKeyToAccount((process.env.NEXT_PUBLIC_HACKED_PRIVATE_KEY as `0x`) || "");
 
 export const publicClient = createPublicClient({
   chain: baseSepolia,
-  transport: http(process.env.ALCHEMY_URL),
+  transport: http("https://sepolia.base.org"),
 });
 
 const walletClient = createWalletClient({
   account,
   chain: baseSepolia,
-  transport: http(process.env.ALCHEMY_URL),
+  transport: http("https://sepolia.base.org"),
 });
 
 export async function mintNft(toAddress: string) {
@@ -22,7 +23,7 @@ export async function mintNft(toAddress: string) {
     const { request }: any = await publicClient.simulateContract({
       account,
       address: contractAddress,
-      abi: contractAbi.contracts.ERC20Portal.abi,
+      abi: config.contracts.ERC20Portal.abi,
       functionName: "mint",
       args: [toAddress, 0, 1, `0x`],
     });
@@ -38,7 +39,7 @@ export async function balanceOf(address: string) {
   try {
     const balanceData = await publicClient.readContract({
       address: contractAddress,
-      abi: contractAbi.contracts.ERC20Portal.abi,
+      abi: config.contracts.ERC20Portal.abi,
       functionName: "balanceOf",
       args: [address as `0x`, 0]
     });
